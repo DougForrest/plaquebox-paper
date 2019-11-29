3a4cf76903658671b3c6dbd2dd6d2ec178bb6c6c
Doug <dougforrest0@gmail.com>
2019-11-29 14:37:28 -0500

    util to create data transfer file for gcp

---
+++ utils/create_data_transfer_file.py
@@ -0,0 +1,18 @@
+import requests
+
+urls = ["https://zenodo.org/record/1470797/files/Dataset%201a%20Development_train.zip?download=1",
+        "https://zenodo.org/record/1470797/files/Dataset%202%20Hold-out.zip?download=1",
+        "https://zenodo.org/record/1470797/files/Dataset%203%20CERAD-like%20hold-out.zip?download=1"]
+
+
+# outfile=
+f = open("downloadfiles.txt", "w")
+f.write("TsvHttpData-1.0")
+for url in urls:
+    response = requests.head(url)
+    print(url)
+    print(response.headers)
+    data = response.headers
+    # print(response.headers['Content-Length'])
+    f.write("\n")
+    f.write("{}      {}      {}".format(url, data['Content-Length'], data['ETag']))
