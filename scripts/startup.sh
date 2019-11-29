e70d1106c220039ae6580668cbfa3c8537e46f22
Doug <dougforrest0@gmail.com>
2019-11-29 14:34:24 -0500

    adding startup script

---
+++ scripts/startup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Update fast ai library
+sudo /opt/anaconda3/bin/conda install -yc fastai fastai
+
+sudo apt-get install -y libvips
+pip install pyvips
+
+cd ~
+git clone https://github.com/DougForrest/plaquebox-paper.git
+
+export MOUNT_DIR=disk-1
+export DEVICE_ID=sdb
+sudo mkdir -p /mnt/disks/$MOUNT_DIR
+sudo mount -o discard,defaults /dev/$DEVICE_ID /mnt/disks/$MOUNT_DIR
