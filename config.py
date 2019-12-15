e94fcf0c510d69f96fed18cc07fab3084a36af44
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-15 06:18:57 +0000

    adding paths

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
 import os 
 
 data_dir = os.path.join('/mnt', 'disks', 'disk-1', 'data')
+csv_dir = os.path.join('data', 'CSVs')
+models_dir = os.path.join(data_dir, 'models')
+csv_path = {
+    'train': os.path.join(csv_dir, 'tiles_train.csv'),
+    'validation': os.path.join(csv_dir, 'tiles_validation.csv'),
+    'test': os.path.join(csv_dir, 'tiles_test.csv'),
+}
+
+img_path = os.path.join(data_dir,
+                        'tiles')
+
+img_path_test = os.path.join(data_dir,
+                        'tiles', 'tiles')
